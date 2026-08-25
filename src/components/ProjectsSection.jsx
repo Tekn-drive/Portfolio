1bba809cfce80b2b28fb6ade12b48b67b3811aca
Tekn-drive <juanwishnutopia@gmail.com>
2026-08-25 12:17:13 +0700

    Added Clickable Links in Project Section

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,4 +1,5 @@
-import '../App.css'
+import '../App.css';
+import { Link } from 'react-router-dom';
 
 export default function ProjectsSection() {
     return (
@@ -7,26 +8,30 @@ export default function ProjectsSection() {
                 <article className="project-category-card project-details" style={{ '--card-delay': '320ms' }}>
                     <h2>Data Mining</h2>
                     <div className="project-list">
-                        <article className="project-item">
-                            <h3>Classification and Regression</h3>
-                            <p>
-                                Performed both classification and regression modeling tasks to
-                                process water potability datasets, performing data extraction,
-                                transformation, and loading into SQL databases.
-                            </p>
-                        </article>
+                        <Link to = "https://github.com/Tekn-drive/ClassificationRegression">
+                            <article className="project-item">
+                                <h3>Classification and Regression</h3>
+                                <p>
+                                    Performed both classification and regression modeling tasks to
+                                    process water potability datasets, performing data extraction,
+                                    transformation, and loading into SQL databases.
+                                </p>
+                            </article>
+                        </Link>
                     </div>
                 </article>
 
                 <article className="project-category-card project-details" style={{ '--card-delay': '670ms' }}>
                     <h2>Object Oriented Programming</h2>
                     <div className="project-list">
-                        <article className="project-item">
-                            <h3>le Hotelier</h3>
-                            <p>
-                                A Java-based hotel management software for handling hotel operations.
-                            </p>
-                        </article>
+                        <Link to = "https://github.com/Tekn-drive/Le-Hotelier">
+                            <article className="project-item">
+                                <h3>Le Hotelier</h3>
+                                <p>
+                                    A Java-based hotel management software for handling hotel operations.
+                                </p>
+                            </article>
+                        </Link>
                     </div>
                 </article>
             </section>
@@ -35,56 +40,65 @@ export default function ProjectsSection() {
                 <article className="project-category-card project-details" style={{ '--card-delay': '1020ms' }}>
                     <h2 id="nlp-title">Natural Language Processing</h2>
                     <div className="project-list">
-                        <article className="project-item">
-                            <h3>Sentiment Analysis</h3>
-                            <p>
-                                A simple React.js-based web app that is able to do sentiment analysis on a given text.
-                            </p>
+                        <Link to = "https://github.com/Tekn-drive/Sentiment-Analysis">
+                            <article className="project-item">
+                                <h3>Sentiment Analysis</h3>
+                                <p>
+                                    A simple React.js-based web app that is able to do sentiment analysis on a given text.
+                                </p>
                             </article>
+                        </Link>
                         
-                        <article className="project-item">
-                            <h3>Fraud Detector</h3>
-                            <p>
-                                A Python desktop application with ETL workflows and natural
-                                language processing to identify fraudulent e-commerce reviews,
-                                improving detection accuracy.
-                            </p>
-                        </article>
+                        <Link to = "https://github.com/Tekn-drive/Fraud-Detector">
+                            <article className="project-item">
+                                <h3>Fraud Detector</h3>
+                                <p>
+                                    A Python desktop application with ETL workflows and natural
+                                    language processing to identify fraudulent e-commerce reviews,
+                                    improving detection accuracy.
+                                </p>
+                            </article>
+                        </Link>
                     </div>
                 </article>
 
                 <article className="project-category-card project-details" style={{ '--card-delay': '1370ms' }}>
                     <h2>Computer Vision</h2>
                     <div className="project-list">
-                        <article className="project-item">
-                            <h3>MSDD</h3>
-                            <p>
-                                Object detection dataset consisting of 300 shelf images in
-                                Indonesian minimarkets containing nearly 14,000 product
-                                instances with more than 100 unique SKUs, each with their own
-                                class labels.
-                            </p>
-                        </article>
-                        
-                        <article className="project-item">
-                            <h3>Real-time Emotion Recognition</h3>
-                            <p>
-                                Real-time emotion recognition pipeline using Python, OpenCV,
-                                and trained datasets, enabling webcam-based data ingestion,
-                                processing, and classification with high data quality.
-                            </p>
-                        </article>
-                        
-                        <article className="project-item">
-                            <h3>Emotion Recognition</h3>
-                            <p>
-                                Image-based emotion classification models developed by applying
-                                data preprocessing, feature extraction, and supervised learning
-                                techniques for accurate predictions. This is a comparative study of different machine learning algorithms for emotion recognition.
-                            </p>
+                        <Link to = "https://github.com/Tekn-drive/MSDD">
+                            <article className="project-item">
+                                <h3>MSDD</h3>
+                                <p>
+                                    Object detection dataset consisting of 300 shelf images in
+                                    Indonesian minimarkets containing nearly 14,000 product
+                                    instances with more than 100 unique SKUs, each with their own
+                                    class labels.
+                                </p>
+                            </article>
+                        </Link>
 
-                        </article>
-                  </div>
+                        <Link to = "https://github.com/Tekn-drive/Realtime-Emotion-Recognition">
+                            <article className="project-item">
+                                <h3>Real-time Emotion Recognition</h3>
+                                <p>
+                                    Real-time emotion recognition pipeline using Python, OpenCV,
+                                    and trained datasets, enabling webcam-based data ingestion,
+                                    processing, and classification with high data quality.
+                                </p>
+                            </article>
+                        </Link>
+                        
+                        <Link to = "https://www.mediafire.com/file/md8cytkn464yb5g/ML_LA02_1_Comparison_Analysis_of_Emotion_Recognition_Models.zip/file">
+                            <article className="project-item">
+                                <h3>Emotion Recognition</h3>
+                                <p>
+                                    Image-based emotion classification models developed by applying
+                                    data preprocessing, feature extraction, and supervised learning
+                                    techniques for accurate predictions. This is a comparative study of different machine learning algorithms for emotion recognition.
+                                </p>
+                            </article>
+                        </Link>
+                    </div>
                 </article>
             </section>
         </div>
